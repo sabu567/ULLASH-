a9fcbed6567ceecbd6fb19614717810b7408d5a1
Sabbir ahmed <sabbir11222w@gmail.com>
2025-03-14 10:51:26 +0600

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -5,32 +5,71 @@ const path = require('path');
 
 module.exports = {
 	config: {
-			name: "owner",
-					aliases: ["info"],
-							author: "Rexy",
-									role: 0,
-											shortDescription: " ",
-													longDescription: "",
-															category: "admin",
-																	guide: "{pn}"
-																		},
+		name: "owner",
+		aliases: ["info"],
+		author: "sabu",
+		role: 0,
+		shortDescription: " ",
+		longDescription: "",
+		category: "admin",
+		guide: "{pn}"
+	},
+
 	onStart: async function ({ api, event }) {
-			try {
-						const ownerInfo = {
-										name: '𝗦𝗔𝗕𝗕𝗜𝗥 𝗔𝗛𝗠𝗘𝗗 ',
-														class: '𝘾𝙡𝙖𝙨𝙨  𝟭𝟭',
-																		group: '𝑺𝑪𝑰𝑬𝑵𝑪𝑬',
-																						gender: '𝑴𝑨𝑳𝑬',
-																										Birthday: '𝟮𝟭-𝟬𝟲-𝟮𝟬𝟬𝟲',
-																														religion: '𝑰𝑺𝑳𝑨𝑴',
-																																		hobby: '𝑭𝒍𝒊𝒓𝒕𝒊𝒏𝒈 😁',
-																																						Fb: 'https://www.facebook.com/share/168jVrRn33/',
-																																										Relationship: '𝙨𝙞𝙣𝙜𝙡𝙚  ',
-																																														Height: '5"9'
-																																																	};
+		try {
+			const ownerInfo = {
+				name: '𝗦𝗔𝗕𝗕𝗜𝗥 𝗔𝗛𝗠𝗘𝗗 ',
+				class: '𝘾𝙡𝙖𝙨𝙨 𝟭𝟭 ',
+				group: '𝑺𝑪𝑰𝑬𝑵𝑪𝑬',
+				gender: '𝑴𝑨𝑳𝑬',
+				Birthday: '𝟮𝟭-𝟬𝟲-𝟮𝟬𝟬𝟲',
+				religion: '𝑰𝑺𝑳𝑨𝑴',
+				hobby: '𝑭𝒍𝒊𝒓𝒕𝒊𝒏𝒈 😁',
+				Fb: 'https://www.facebook.com/profile.php?id=61566551318414',
+				Relationship: '𝙨𝙞𝙣𝙜𝙡𝙚 ',
+				Height: '𝟱"𝟵'
+			};
+
 			const bold = 'https://i.imgur.com/DpMSasE.mp4';
-						const tmpFolderPath = path.join(__dirname, 'tmp');
+			const tmpFolderPath = path.join(__dirname, 'tmp');
+
 			if (!fs.existsSync(tmpFolderPath)) {
-							fs.mkdirSync(tmpFolderPath);
-										}
+				fs.mkdirSync(tmpFolderPath);
+			}
+
 			const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
+			const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
+
+			fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
+
+			const response = `
+𓀬 𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎 𓀬 \n
+ ~𝙉𝘼𝙈𝙀: ${ownerInfo.name}
+ ~𝘾𝙇𝘼𝙎𝙎: ${ownerInfo.class}
+ ~𝙂𝙍𝙊𝙐𝙋: ${ownerInfo.group}
+ ~𝙂𝙀𝙉𝘿𝙀𝙍: ${ownerInfo.gender}
+ ~𝘽𝙄𝙍𝙏𝙃𝘿𝘼𝙔: ${ownerInfo.Birthday}
+ ~𝙍𝙀𝙇𝙄𝙂𝙄𝙊𝙉: ${ownerInfo.religion}
+ ~𝙍𝙀𝙇𝘼𝙏𝙄𝙊𝙉𝙎𝙃𝙄𝙋: ${ownerInfo.Relationship}
+ ~𝙃𝙊𝘽𝘽𝙔: ${ownerInfo.hobby}
+ ~𝙃𝙀𝙄𝙂𝙃𝙏: ${ownerInfo.Height}
+ ~𝙁𝘽: ${ownerInfo.Fb}
+			`;
+
+			await api.sendMessage({
+				body: response,
+				attachment: fs.createReadStream(videoPath)
+			}, event.threadID, event.messageID);
+
+			fs.unlinkSync(videoPath);
+
+			api.setMessageReaction('😘', event.messageID, (err) => {}, true);
+		} catch (error) {
+			console.error('Error in ownerinfo command:', error);
+			return api.sendMessage('An error occurred while processing the command.', event.threadID);
+		}
+	}
+};
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
